9d60014f65171d3130d12efca6668fdb0ff754ee
wujie1416 <18911578088@163.com>
2017-01-18 11:44:05 +0800

    first commit

---
+++ BossClientiOS/BossClientiOS.swift
@@ -0,0 +1,13 @@
+//
+//  BossClientiOS.swift
+//  BossClientiOS
+//
+//  Created by seastar on 2017/1/18.
+//  Copyright © 2017年 seastar. All rights reserved.
+//
+
+import UIKit
+
+public class BossClientiOS: NSObject {
+
+}
